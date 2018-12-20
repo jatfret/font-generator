7e222a82eb0b668923a002ea2ddf69c22cffa2f0
jatfret <jatfret@gmail.com>
2018-12-21 01:45:31 +0800

    update: 增加css、html模版

--- bin/bk-font-generator.js
+++ bin/bk-font-generator.js
@@ -2,15 +2,13 @@
 
 const chalk = require('chalk')
 const path = require('path')
-const fs = require('fs-extra')
+const fs = require('fs')
 const program = require('commander')
 const { checkNodeVer } = require('../lib/checkNodeVer.js')
 const { handleError } = require('../lib/handleError.js')
 const { generatorFiles } = require('../lib/generatorFiles.js')
 
 checkNodeVer() // 检查 Node.js 版本
-
-const options = {}
 
 program
     .version(require('../package.json').version, '-v, --version', '查看插件版本')
@@ -25,35 +23,60 @@ program.parse(process.argv)
 let [sourcePath, destPath] = program.args
 
 if (sourcePath) {
-    destPath = destPath || path.join(process.cwd(), 'bkfont/')
-    
+    destPath = destPath || path.join('bkiconfont')
+    const fontPath = path.join(destPath, '/fonts/')
     fs.stat(sourcePath, (err, stat) => {
         if (err) {
             handleError(err)
         }
         
-        let sourceFiles = []
+        const sourceFiles = []
         if (stat.isFile()) {
             if (path.extname(sourcePath) !== 'svg') {
                 handleError('请传入合法的svg文件')
             }
             sourceFiles.push(path.join(sourcePath))
-            generatorFiles(sourceFiles, destPath)
         } else {
-            fs.readdir(sourcePath, (readErr, files) => {
-                if (readErr) {
-                    handleError(readErr)
+            const files = fs.readdirSync(sourcePath)
+            files.forEach(file => {
+                if (path.extname(file) === '.svg') {
+                    sourceFiles.push(path.join(sourcePath, file))
                 }
-                files.forEach(file => {
-                    if (path.extname(file) === '.svg') {
-                        sourceFiles.push(path.join(sourcePath, file))
-                    }
-                })
-                generatorFiles(sourceFiles, destPath)
             })
         }
+        executeTransform(sourceFiles, fontPath)
     })
 } else {
-    handleError('请传入需要转换的源文件和输出目录')
+    handleError('请传入需要转换的svg图标文件和输出目录')
+}
+
+function executeTransform (files, dest) {
+    const fileLen = files.length
+    console.log(chalk.green(`svg图标文件 ${fileLen} 个`))
+    if (fileLen) {
+        const options = getOptions(dest)
+        generatorFiles(files, dest, options)
+    } else {
+        console.log(chalk.yellow('请传入需要转换的svg图标文件'))
+    }
+}
+
+function getOptions (dest) {
+    return {
+        fontName: 'bkicon',
+        css: true,
+        cssDest: path.join(dest, '../demo.css'),
+        cssTemplate: '../templates/css.hbs',
+        cssFontsUrl: path.join('./fonts/'),
+        html: true,
+        htmlDest: path.join(dest, '../demo.html'),
+        htmlTemplate: '../templates/html.hbs',
+        templateOptions: {
+            classPrefix: 'icon-',
+            baseClassName: 'bk-icon'
+        },
+        types: ['svg', 'ttf', 'woff2', 'woff', 'eot'],
+        order: ['svg', 'ttf', 'woff2', 'woff', 'eot']
+    }
 }
 
